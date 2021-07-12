0293138a9224c513dcc0687bdaa6d06eff6a438a
Sinotca529 <sinotca35963@gmail.com>
2021-07-12 18:12:57 +0900

    LinearLgisticのlipsitz連続性のとり方を修正。多値分類はwip

--- objfunc.py
+++ objfunc.py
@@ -1,4 +1,5 @@
 from abc import abstractmethod
+import collections
 import numpy as np
 from numpy import ndarray
 
@@ -28,6 +29,13 @@ def inv_1p_exp(x: ndarray) -> ndarray:
             return m / (1.0 + m)
 
     return np.vectorize(inv_1p_exp_for1d)(x)
+
+# ベクトルを受け取り、各要素をexpしたものの和の、logをとる。
+# Overflowが起きるなら、式変換した(まともな)実装に直す必要あり。
+def log_sum_exp(x: ndarray) -> float:
+    assert((x.shape == (x.shape[0],)) or (x.shape[1] == 1))
+    lse = np.log(np.sum(np.exp(x)))
+    return lse
 
 class ObjFunc():
     # データセットを用いたパラメタの設定などが必要な場合はこれを用いる
@@ -104,28 +112,108 @@ class LinearLogistic(ObjFunc):
 
     def get_grad_lipsitz(self, data_set: DataSet) -> float:
         x = data_set.x
-        acc = 0.0
-        for i in range(0, data_set.qty_sample):
-            xi = x[i,:].reshape((1, data_set.dim))
-            acc += xi.dot(xi.T)[0,0]
-
-        lip = acc / 4.0 + 2 * self.__lambda
-        # print("lip : ", str(lip))
+        lip = np.trace(x.dot(x.T)) / 4.0 + 2.0 * self.__lambda
         return lip
 
-    # def test_apply(self):
-    #     dset = test_data()
-    #     w = np.array([
-    #         [7],
-    #         [8],
-    #         [9]
-    #     ])
+# 重みwは、各行が各カテゴリの重みに対応。
+# xは、各行が各サンプルに対応
+# yは、各行が各サンプルのカテゴリに対応
+class MulticlassLogisticRegression(ObjFunc):
+    def setup_with_data_set(self, data_set: DataSet) -> float:
+        self.__lambda = 0.01 * data_set.qty_sample
 
-    #     p1 = 1/(1 + np.exp(-50))
-    #     p2 = 1/(1 + np.exp(122))
-    #     print("priterior: (" + str(p1) + ", " + str(p2) + ")")
-    #     print(self.__calc_posterior(dset, w))
+    # todo : 実装の確認
+    def apply(self, data_set: DataSet, w: ndarray) -> float:
+        (x, y) = (data_set.x, data_set.y)
+        dim = data_set.dim
+        qty_c = data_set.qty_category
+        qty_sample = data_set.qty_sample
+        assert(w.shape == (qty_c, dim))
 
-    #     print("apply:")
-    #     print(np.log1p(np.exp(-50)) + np.log1p(np.exp(122)) + self.__lambda * w.T.dot(w))
-    #     print(self.apply(dset, w))
+        # Σ[i=0..n] ln(Σ[c=0..C] exp(<w_c, x_i>))
+        # を求める
+        lse = np.apply_along_axis(log_sum_exp, 1, x.dot(w.T))
+        assert(lse.shape == (qty_sample, 1))
+        slse = np.sum(lse)
+
+        # Σ[i=0..n] -<w_y(i) x_i>
+        # を求める
+        acc = 0.0
+        for i in range(0, qty_sample):
+            ci = y[i]
+            wyi = w[ci]
+            xi = x[:, i]
+            acc += -wyi.dot(xi)[0,0]
+        
+        reg = np.linalg.norm(w, ord=2)
+        loss = slse + acc + reg
+        return loss
+
+    # retval[r, i] = p(r | x_i)
+    def __calc_posterior(self, data_set: DataSet, w: ndarray) -> ndarray:
+        (x, y) = (data_set.x, data_set.y)
+        dim = data_set.dim
+        qty_c = data_set.qty_category
+        qty_sample = data_set.qty_sample
+        assert(w.shape == (qty_c, dim))
+
+        # 分子: numerator[r, i] = exp(<w_r, x_i>)
+        numerator = np.exp(w.dot(x.T))
+        assert(numerator.shape == (qty_sample, qty_c))
+
+        # 分母: denominator[i] = Σ_c exp(<w_c, x_i>)
+        denominator = np.sum(numerator, axis=0)
+
+        posterior =  numerator/denominator
+        assert(posterior.shape == (qty_sample, 1))
+        return posterior
+
+    # 各行が各カテゴリの重みの勾配に相当
+    def apply_grad(self, data_set: DataSet, w: ndarray) -> ndarray:
+        (x, y) = (data_set.x, data_set.y)
+        dim = data_set.dim
+        qty_c = data_set.qty_category
+        qty_sample = data_set.qty_sample
+        assert(w.shape == (qty_c, dim))
+
+        posterior = self.__calc_posterior(data_set, w)
+
+        # yr[r, i] = [[ y_i == r ]] を作る
+        yr = [[int(i==y[j]) for j in range(0, y.shape[0])] for i in range(0, qty_c)]
+        assert(yr.shape == (qty_c, qty_sample))
+
+        grad = (posterior - yr).dot(x) + 2 * self.__lambda * w
+
+        # wi = x( p(i|x) - [[ y==i ]] )
+        pass
+
+    def apply_hessian(self, data_set: DataSet, w: ndarray) -> ndarray:
+        (x, y) = (data_set.x, data_set.y)
+        dim = data_set.dim
+        qty_c = data_set.qty_category
+        qty_sample = data_set.qty_sample
+        assert(w.shape == (qty_c, dim))
+
+        # xx[i] = (x_i.T)(x_i)な3次元配列を作る
+        xx = np.array([x[i:i+1].T.dot(x[i:i+1]) for i in range(0, qty_sample)])
+
+        posterior = self.__calc_posterior(data_set, w)
+
+        # hessian[i] = w_r用のヘッシアン
+        hessian = np.zeros((qty_c, dim, dim))
+        # w_rに関する部分を個別に計算
+        for r in range(0, qty_c):
+            p1mp = posterior * (1 - posterior)
+            hes_r = np.zeros((dim, dim))
+            for i in range(0, qty_sample):
+                hes_r += p1mp[r, i] * xx[i]
+            hes_r += 2 * self.__lambda
+            hessian[r] = hes_r
+
+        assert(hessian.shape == (qty_c, dim, dim))
+        return hessian
+
+    def get_grad_lipsitz(self, data_set: DataSet) -> float:
+        x = data_set.x
+        lip = np.trace(x.dot(x.T)) / 4.0 + 2.0 * self.__lambda
+        return lip
